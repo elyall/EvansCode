efa440ac6c9309b9fe297146a263b0fad1ddda1e
Evan Lyall <evan.lyall@gmail.com>
2018-02-16 10:21:02 -0800

    made CI computation reproducible

--- Analyses/TuningCurves/computeBootStrappedCI.m
+++ Analyses/TuningCurves/computeBootStrappedCI.m
@@ -3,6 +3,8 @@ function [CI95,ROIs] = computeBootStrappedCI(Raw,varargin)
 ROIs = {};
 ROIindex = [];
 FileIndex = [];
+Type = 'cper';  % 'norm', 'per', 'cper', 'bca', or 'stud' (see: BOOTCI)
+Seed = 73;      % seed for random # generator
 
 saveOut = false;
 saveFiles = {};
@@ -24,6 +26,12 @@ while index<=length(varargin)
                 index = index + 2;
             case 'FileIndex'
                 FileIndex = varargin{index+1};
+                index = index + 2;
+            case 'Type'
+                Type = varargin{index+1};
+                index = index + 2;
+            case 'Seed'
+                Seed = varargin{index+1};
                 index = index + 2;
             case {'save','Save'}
                 saveOut = true;
@@ -52,16 +60,21 @@ end
 
 %% Compute 95% CI
 
+% Set random number generator so that outputs are reproducible
+stream = RandStream('mlfg6331_64','Seed',Seed);
+RandStream.setGlobalStream(stream);
+% opts = statset('UseParallel',true,'UseSubstreams',true,'Streams',stream); % slower than parfor for whatever reason
+opts = statset('Streams',stream);
+
 [numROIs,numStim] = size(Raw);
 fprintf('Computing bootstrapped confidence intervals for %d ROIs...',numROIs);
 
 CI95 = nan(numROIs,2,numStim);
 parfor ind = 1:numROIs
     for sind = 1:numStim
-        CI95(ind,:,sind) = bootci(10000,{@mean,Raw{ind,sind}},'type','bca'); % bootstrapped confidence intervals of the mean
+        reset(stream); % reset random # generator
+        CI95(ind,:,sind) = bootci(10000,{@mean,Raw{ind,sind}},'type',Type,'Options',opts); % bootstrapped confidence intervals of the mean
     end
-%     b = cellfun(@(x) bootci(10000,{@mean,x},'type','bca'), Raw(ind,:),'UniformOutput',false);
-%     CI95(ind,:,:) = cat(2,b{:});
 end
 
 fprintf('\tComplete\n');
@@ -75,11 +88,13 @@ if (nargout>1 || (saveOut && ~isempty(saveFiles))) && ~isempty(ROIs)
     if saveOut
         for ind = 1:numel(ROIs)
             ROIdata = ROIs{ind};
+            fprintf('Saving ROIdata to: %s...',saveFiles{ind});
             if ~exist(saveFiles{ind},'file')
                 save(saveFiles{ind},'ROIdata','-v7.3');
             else
                 save(saveFiles{ind},'ROIdata','-append');
             end
+            fprintf('\tComplete\n');
         end
     end
 end
