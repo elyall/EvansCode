23dfae994c8045cd6756391ca5856e21df1fb8a7
Evan Lyall <evan.lyall@gmail.com>
2019-04-19 14:32:34 -0700

    commented code

--- Tools/saveEPS.m
+++ Tools/saveEPS.m
@@ -1,4 +1,7 @@
 function saveEPS(fn,hF)
+% SAVEEPS saves figure to .eps file
+%   fn (string) - filename to save figure to (default: 'temp.eps')
+%   hF (figure handle) - handle of figure to save (default: current figure)'
 
 if ~exist('fn','var') || isempty(fn)
     fn = 'temp.eps';
--- Tools/savePDF.m
+++ Tools/savePDF.m
@@ -1,7 +1,10 @@
 function savePDF(fn,hF)
+% SAVEPDF saves figure to .pdf file
+%   fn (string) - filename to save figure to (default: 'temp.pdf')
+%   hF (figure handle) - handle of figure to save (default: current figure)
 
 if ~exist('fn','var') || isempty(fn)
-    fn = 'temp.eps';
+    fn = 'temp.pdf';
 end
 if ~exist('hF','var') || isempty(hF)
     hF = gcf;
